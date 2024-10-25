9e6262635617cd357f25d293d00bd4692af9bcd0
Giorgi Beridze <gioberidzegio@yahoo.com>
2024-10-25 08:43:40 +0400

    replaced tar command with 7zip.exe in  prepare.bat

--- prepare.bat
+++ prepare.bat
@@ -5,5 +5,6 @@ curl -o static\js\alpine.js https://cdn.jsdelivr.net/npm/alpinejs@3.14.3/dist/cd
 curl -o static\js\persist.js https://cdn.jsdelivr.net/npm/@alpinejs/persist@3.14.3/dist/cdn.js
 curl -o static\js\alpinejs-swipe.js https://unpkg.com/alpinejs-swipe@1.0.2/dist/cjs.js
 curl -L -O https://github.com/mindiae/ourbible/releases/download/0.22.5/database.tar.gz
-tar -xzvf database.tar.gz
+7z.exe x database.tar.gz
+7z.exe x database.tar
 del database.tar.gz
