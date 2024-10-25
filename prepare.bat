1e201223c0d2a429cdfea134aab660b5edc2fe65
Giorgi Beridze <gioberidzegio@yahoo.com>
2024-10-25 08:31:30 +0400

    added prepare.bat batch file for windows

---
+++ prepare.bat
@@ -0,0 +1,9 @@
+@echo off
+
+mkdir static\js
+curl -o static\js\alpine.js https://cdn.jsdelivr.net/npm/alpinejs@3.14.3/dist/cdn.js
+curl -o static\js\persist.js https://cdn.jsdelivr.net/npm/@alpinejs/persist@3.14.3/dist/cdn.js
+curl -o static\js\alpinejs-swipe.js https://unpkg.com/alpinejs-swipe@1.0.2/dist/cjs.js
+curl -L -O https://github.com/mindiae/ourbible/releases/download/0.22.5/database.tar.gz
+tar -xf database.tar.gz
+del database.tar.gz
